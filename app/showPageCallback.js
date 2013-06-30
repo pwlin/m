21281d09311bb63bb466e55bca8df95a3d066ac7
pwlin <pwlin05@gmail.com>
2013-06-30 14:13:18 +0200

    corrected spelling

--- app/showPageCallback.js
+++ app/showPageCallback.js
@@ -11,7 +11,7 @@ function showAboutPage() {
     content = '';
     content += '<ul class="items">';
     content += '<li>This is a simple Feed Reader with the Help of Google Feed API.</li>';
-    content += '<li>The complete source code of this app is <a href="https://github.com/pwlin/m">hosted on Github</a>.</li>';
+    content += '<li>The complete source code of this app is <a href="https://github.com/pwlin/m">hosted on GitHub</a>.</li>';
     content += '</ul>';
     $('article#maincolumn').innerHTML = content;
 };

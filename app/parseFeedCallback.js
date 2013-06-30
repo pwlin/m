a92352fe0f9e63be43848388e1316f62c609ed77
pwlin <pwlin05@gmail.com>
2013-06-30 23:38:59 +0200

    * Cleaned up m.feeds.js * function to format the entry before adding to DOM

--- app/parseFeedCallback.js
+++ app/parseFeedCallback.js
@@ -7,11 +7,37 @@ function parseFeedCallback(feedKey, response) {
     content += '<li style="color:' + textColor + ';">Latest Entries In ' + feed['feedName'] + ':</li>';
     var entries = response['feed']['entries'];
     for (var i = 0, k = entries.length; i < k ; i++) {
-        content += '<li><a style="color:' + textColor + ';" href="' + entries[i]['link'] + '">&rsaquo;&rsaquo; ' + entries[i]['title'] + '</a></li>';
+        var entry = handleExtraContent(feed, entries[i]);
+        content += '<li><a style="color:' + textColor + ';" href="' + entry['link'] + '">&rsaquo;&rsaquo; ' + entry['title'] + '</a> ' + entry['extraContent'] + '</li>';
     };
     content += '</ul>';
     $('article#maincolumn').innerHTML = content;
     if ($('article#maincolumn').offsetHeight < $('section#sidebar').offsetHeight) {
         $('article#maincolumn').style.height = ($('section#sidebar').offsetHeight) + 'px';    
     };
+};
+
+function handleExtraContent(feed, entry) {
+    entry['extraContent'] = '';
+    var isMobileUser = navigator.userAgent.match(/Android|iPhone/i) ? true : false;
+    switch(feed['feedName']) {
+        case 'HN':
+            entry['extraContent'] = entry['content'].replace('<a', '<a style="margin-left:5px;color:#FFFFFF;font-size:smaller" ');
+            entry['extraContent'] = entry['extraContent'].replace('Comments', '[ Comments ]');
+            if (isMobileUser === true) {
+                entry['extraContent'] = entry['extraContent'].replace(/https:\/\/news\.ycombinator\.com\/item\?id=/, 'http://ihackernews.com/comments/');    
+            };
+        break;
+        
+        case '/r/JavaScript':
+        case '/r/CSS':
+        case '/r/PHP':
+        case '/r/ShutupAndTakeMyMoney':
+            if (isMobileUser === true) {
+                entry['link'] = entry['link'].replace(/\/$/, '/.compact');    
+            };
+        break;
+    
+    };
+    return entry;
 };
